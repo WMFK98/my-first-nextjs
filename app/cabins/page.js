dca8de41198b4c9a7ced9e0bdf87593b4715add5
realFK98 <wishira2013.coth@gmail.com>
2024-09-07 23:46:32 +0700

    try the magic fetch in async component RSC XD

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <h1></h1>;
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
